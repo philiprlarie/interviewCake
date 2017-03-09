69c4cbc5fb314bcb49e5e421f7969e55bc1790da
philiprlarie <philiprlarie@gmail.com>
2017-03-09 09:22:05 -0800

    clean up

--- src/Gcd.java
+++ src/Gcd.java
@@ -16,3 +16,7 @@ public class Gcd {
         return larger;
     }
 }
+
+////        System.out.println(Gcd.euclidsAlgo(100, 75)); // should be 25
+////        System.out.println(Gcd.euclidsAlgo(99, 60)); // should be 3
+////        System.out.println(Gcd.euclidsAlgo(225, 345)); // should be 15
--- src/TestProblem5.java
+++ src/TestProblem5.java
@@ -37,3 +37,34 @@ public class TestProblem5 {
         assertEquals(expectedNumWays, actualNumWays);
     }
 }
+
+
+//        System.out.println(CreateChange.createChange(4, new int[] {1, 2, 3})); // should be 4
+//        System.out.println(CreateChange.createChange(10, new int[] {2, 5, 3, 6})); // should be 5
+////        https://www.codechef.com/problems/CHANGE
+////        System.out.println(CreateChange.createChange(380134, new int[] {2, 5, 23}));
+////        long startTime = System.nanoTime();
+////        System.out.println(CreateChange.createChange(12000, new int[] {6, 5, 2, 8, 1, 4, 7, 3}) % 1000000007); // should be 314159265
+////        long endTime = System.nanoTime();
+//
+////        long duration = (endTime - startTime) / 100000000;
+////        System.out.println(duration);
+//
+//
+//        long startTime;
+//        long endTime;
+//        long duration;
+//
+//        startTime = System.nanoTime();
+//        System.out.println(CreateChange.createChange(10010, new int[] {4, 3, 2, 5}));
+//        endTime = System.nanoTime();
+//        duration = (endTime - startTime) / 1000000;
+//        System.out.println(duration);
+//
+//        startTime = System.nanoTime();
+//        System.out.println(CreateChange.createChange2(10010, new int[] {4, 3, 2, 5}));
+//        endTime = System.nanoTime();
+//        duration = (endTime - startTime) / 1000000;
+//        System.out.println(duration);
+//
+
